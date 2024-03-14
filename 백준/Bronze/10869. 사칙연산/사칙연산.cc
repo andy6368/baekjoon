bfe3685c855632989b919989d014dac602b842d9
andy6368 <69963070+andy6368@users.noreply.github.com>
2024-03-14 15:22:57 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	int A, B;
+	std::cin >> A;
+	std::cin >> B;
+	std::cout << A + B <<std::endl;
+	std::cout << A - B << std::endl;
+	std::cout << A * B << std::endl;
+	std::cout << A / B << std::endl;
+	std::cout << A % B;
+	return 0;
+	
+}
